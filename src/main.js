7233a912c0eca4c036713914f6f54e711f13aec3
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-20 19:41:14 +0100

    updates

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+inView(".box1", () => {
+  animate(".box1", { x: [-2000, 0] }, { duration: 3 });
+});
+
+inView(".box2", () => {
+  animate(".box2", { x: [-8500, 0] }, { duration: 5 });
+});
